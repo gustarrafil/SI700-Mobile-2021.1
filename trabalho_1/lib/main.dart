d111015c358984cc9d9b604a40399878ab4ff6f7
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2021-04-13 21:18:41 -0300

    organizacao

---
+++ trabalho_1/lib/main.dart
@@ -0,0 +1,17 @@
+/**
+ * Arthur Briganti Gini 213253
+ * Gustavo Tarrafil Sousa Pereira 159740
+ */
+
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        //return MyTabBarLayout();
+    }
+}
